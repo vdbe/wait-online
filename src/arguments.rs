8928b8afebe03d206be1224c14b9441e6013e76a
vdbewout <vdbewout@gmail.com>
2023-09-09 21:53:18 +0200

    argument documentation

--- src/arguments.rs
+++ src/arguments.rs
@@ -4,33 +4,48 @@ use clap::{value_parser, Parser};
 #[derive(Debug)]
 #[cfg_attr(feature = "clap", derive(Parser), command(author, version, about))]
 pub struct Args {
-    /// Interface(s) to wait for
+    /// Block until at least these interfaces have appeared
     #[cfg_attr(feature = "clap", arg(short, long, conflicts_with = "ignore"))]
     pub interface: Option<Vec<String>>,
 
+    /// Don't take these interfaces into account
+    ///
+    /// By default only loopback interfaces are ignored.
     #[cfg_attr(feature = "clap", arg(long, conflicts_with = "interface"))]
     pub ignore: Option<Vec<String>>,
 
-    /// Max time before failing in seconds
-    #[cfg_attr(feature = "clap", arg( long, default_value_t = Self::DEFAULT_TIMOUT))]
-    pub timeout: u64,
-
+    /// Requires at least one IPv4 address
     #[cfg_attr(
         feature = "clap",
         arg(short = '4', long, default_value_t = false)
     )]
     pub ipv4: bool,
 
+    /// Requires at least one IPv6 address
     #[cfg_attr(
         feature = "clap",
         arg(short = '6', long, default_value_t = false)
     )]
     pub ipv6: bool,
 
+    /// Wait until at least one of the interfaces is online
+    ///
+    /// If this options is specified with `--interface`, then wait until at
+    /// least one specified interfaces becomes online.
     #[cfg_attr(feature = "clap", arg(long, default_value_t = false))]
     pub any: bool,
 
+    /// Maximum time to wait for network connectivity in seconds
+    ///
+    /// Fail the service if the network is not online by the time the timeout
+    /// elapses.
+    /// A timeout of 0 disables the timeout.
+    #[cfg_attr(feature = "clap", arg( long, default_value_t = Self::DEFAULT_TIMOUT))]
+    pub timeout: u64,
+
     /// Time between checks in ms
+    ///
+    /// Must be between inclusive 10 and 10_000.
     #[cfg_attr(feature = "clap", arg(
         long, default_value_t = Self::DEFAULT_INTERVAL,
         value_parser = value_parser!(u64).range(Self::MIN_INTERVAL..=Self::MAX_INTERVAL)
